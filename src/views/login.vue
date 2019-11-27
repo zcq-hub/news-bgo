e07fe56137c433a257e30bc680c7e1f6f1c8fac5
zcq <1033314450@qq.com>
2019-11-27 14:32:41 +0800

    01-配置好项目的结构

---
+++ src/views/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="less">
+</style>
